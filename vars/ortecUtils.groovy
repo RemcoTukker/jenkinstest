11579e6c877a43877d838238e27a5ac66f63e944
RemcoTukker <helium_2@hotmail.com>
2016-12-13 16:55:33 +0100

    Update ortecUtils.groovy

--- vars/ortecUtils.groovy
+++ vars/ortecUtils.groovy
@@ -1,10 +1,76 @@
-// vars/acme.groovy
-def setFoo(v) {
-    this.foo = v;
+String mountWorkspace(String mountPoint) {
+    if (mountPoint == '') {
+        mountPoint = '/src2'
+    }
+    def wscygwin = env.WORKSPACE.replaceAll("\\\\","/")
+    
+    def cygwincmd = 'echo \'' + wscygwin + ' ' + mountPoint + ' ntfrs text,noacl,posix=0,user 0 0\' >> /etc/fstab ; mount -a ; '
+    executeInCygwin(cygwincmd, '') // TODO check if succeeded...
+    return mountPoint
 }
-def getFoo() {
-    return this.foo;
+
+int executeInCygwin(String bashScript, String location) {
+    if (location == '') {
+        location = '/'  // I tried ~ , but apparently the user is SYSTEM... Still the case with better mount? TODO fix! 
+    }
+    def cygwincmd = 'PATH=/usr/bin:$PATH ; cd ' + location + ' ; ' + bashScript
+    bat 'C:\\cygwin\\bin\\bash.exe -c "' + cygwincmd + '"'
+    return 0 // TODO somehow return something relevant... stdout, stderr, err, return code?
 }
-def say(name) {
-    echo "Hello world, ${name}"
+
+void syncSource(String clientSpecDepotLocation) {
+    def workspaceName = "jenkins-${NODE_NAME}-${JOB_NAME}"
+    def clientSpecWorkspaceName = workspaceName + '-clientspec'
+
+    echo 'Retrieving clientspec from Perforce...'
+    checkout([
+        $class: 'PerforceScm', 
+        credential: 'ebfe3457-08af-4b78-8822-33364e7c5c95', 
+        populate: [
+            $class: 'AutoCleanImpl', // TODO we dont have to use this populate option here maybe? we only care about getting the right spec.txt
+            delete: false, 
+            modtime: false, 
+            parallel: [enable: false, minbytes: '1024', minfiles: '1', path: '/usr/local/bin/p4', threads: '4'], 
+            pin: '', 
+            quiet: true, 
+            replace: true
+        ], 
+        workspace: [
+            $class: 'ManualWorkspaceImpl', 
+            charset: 'none', 
+            name: clientSpecWorkspaceName,
+            pinHost: false,
+            spec: [ view: clientSpecDepotLocation + ' //' + clientSpecWorkspaceName + '/spec.txt' ]
+        ]
+    ])
+
+    def clientSpec = readFile file:'spec.txt'
+    mapping = clientSpec.replaceFirst(/(?ms).*^[Vv]iew:/, "")     // remove everything before and including View:
+    mapping = mapping.replaceAll(/(?m)^[ \t]*/, "")               // remove leading whitespace
+    mapping = mapping.replaceAll(/(?m)#.*$/, "")                  // remove comments
+    mapping = mapping.replaceAll(/(?m)^\r?\n/, "")                // remove empty lines
+    mapping = mapping.replaceAll('%COMPUTERNAME%', workspaceName) // fill in the workspace placeholder
+    echo 'mapping: ' + mapping
+    
+    echo 'Syncing from Perforce...'
+    checkout([
+        $class: 'PerforceScm', 
+        credential: 'ebfe3457-08af-4b78-8822-33364e7c5c95', 
+        populate: [
+            $class: 'AutoCleanImpl', 
+            delete: true, 
+            modtime: false, 
+            parallel: [enable: false, minbytes: '1024', minfiles: '1', path: '/usr/local/bin/p4', threads: '4'], 
+            pin: '', 
+            quiet: true, 
+            replace: true
+        ], 
+        workspace: [
+            $class: 'ManualWorkspaceImpl', 
+            charset: 'none', 
+            name: workspaceName,
+            pinHost: false,
+            spec: [ view: mapping ]
+        ]
+    ])
 }
